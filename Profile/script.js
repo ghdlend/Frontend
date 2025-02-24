f28e4fbfe2b9023ac12bbb36065287d511f8cb36
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-02-24 19:05:28 +0900

    정렬,탐색

--- Profile/script.js
+++ Profile/script.js
@@ -1,11 +1,8 @@
 let currentIndex = 0;
 function changeProfile(){
-    const names = ["손흥민","류현진","김연경"];
+    const names = ["손흥민","류현진","김연아"];
     const jobs = ["축구선수","야구선수","피겨스케이팅"];
     const images = ["profile1.jpg","profile2.jpg","profile3.jpg"];
-
-    //랜덤값을 넣고 싶으면 currentIndex 값을 없애고 밑에 문장 삽입
-    //let randomIndex = Math.floor(Math.random() * names.length);
 
     document.getElementById("name").textContent = names[currentIndex];
     document.getElementById("job").textContent = jobs[currentIndex];
